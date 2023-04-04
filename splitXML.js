03a5ef1cbdc067e703364108a39d3fee46de355e
Paul Hayes <fofr@users.noreply.github.com>
2023-04-04 16:50:17 +0100

    Create one file per page
    Ignore certain page types

--- splitXML.js
+++ splitXML.js
@@ -4,13 +4,29 @@ const parser = new xml2js.Parser()
 const args = process.argv.slice(2)
 
 if (args.length < 1) {
-  console.error('Usage: node splitXML.js <input-file> [items-per-file]')
+  console.error('Usage: node splitXML.js <input-file>')
   process.exit(1)
 }
 
 const inputFile = args[0]
 const outputFolder = 'output'
-const itemsPerFile = args.length > 1 ? parseInt(args[1]) : 100
+const excludedPrefixes = [
+  'User:',
+  'User talk:',
+  'User blog:',
+  'User blog comment:',
+  'MediaWiki:',
+  'File:',
+  'Talk:',
+  'Forum:'
+]
+
+const filterPages = (page) => {
+  return (
+    !excludedPrefixes.some(prefix => page.title[0].startsWith(prefix)) &&
+    !page.hasOwnProperty('redirect')
+  )
+}
 
 fs.readFile(inputFile, 'utf8', (err, data) => {
   if (err) {
@@ -24,22 +40,20 @@ fs.readFile(inputFile, 'utf8', (err, data) => {
       return
     }
 
-    const pages = result.mediawiki.page
-    const totalPages = pages.length
-    const totalFiles = Math.ceil(totalPages / itemsPerFile)
+    const pages = result.mediawiki.page.filter(filterPages)
 
     fs.ensureDir(outputFolder)
       .then(() => {
-        for (let i = 0; i < totalFiles; i++) {
-          const start = i * itemsPerFile
-          const end = Math.min(start + itemsPerFile, totalPages)
-          const outputFile = `${outputFolder}/file-${i + 1}.json`
+        pages.forEach(page => {
+          const title = page.title[0].replace(/\//g, '-')
+          const timestamp = page.revision[0].timestamp[0].split('T')[0]
+          const filename = `${title}_${timestamp}`.replace(/[^\w\/]|_/g, '-').toLowerCase()
+          const outputFile = `${outputFolder}/${filename}.json`
 
-          const filePages = pages.slice(start, end)
-          fs.writeJson(outputFile, filePages, { spaces: 2 })
+          fs.writeJson(outputFile, page, { spaces: 2 })
             .then(() => console.log(`Created: ${outputFile}`))
             .catch(err => console.error(`Error writing JSON file: ${err}`))
-        }
+        })
       })
       .catch(err => console.error(`Error creating output folder: ${err}`))
   })
